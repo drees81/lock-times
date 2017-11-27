7330ce19bd0da555472ba60d46ff74bf0a7a913b
Jan Drees <jan@drees81.de>
2017-11-27 19:05:07 +0100

    season tests

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+const seasonStart = new Date("2000-04-15 00:00:00.0");
+const seasonEnd = new Date("2000-10-15 00:00:00.0");
+
 module.exports = {
     calcLockSchedule: function(date) {
-        return []
+        if (module.exports.isInSeason(date)) {
+            return ['x']
+        } else {
+            return []
+        }
+    },
+
+    isInSeason: function(isoDate) {
+        const date = new Date(isoDate);
+        date.setYear(2000)
+        date.setHours(0, 0 ,0 ,0);
+        
+        return date >= seasonStart && date <= seasonEnd;
     }
 };
--- test/index.js
+++ test/index.js
@@ -1,9 +1,48 @@
 var should = require('chai').should()
 var lockTimeCalculator = require('../index')
 var calcLockSchedule = lockTimeCalculator.calcLockSchedule
+var isInSeason = lockTimeCalculator.isInSeason
 
-describe("calcSchedule", function(){
-    it('return no times for a date in January', function() {
-        calcLockSchedule('2018-01-01').length.should.equal(0)
+describe("calcSchedule - out of season", function(){
+    it('last day before the season', function() {
+        calcLockSchedule('2018-04-14').length.should.equal(0)
     })
+    it('first day after the season', function() {
+        calcLockSchedule('2018-10-16').length.should.equal(0)
+    })
+})
+
+
+describe("helper functions", function() {
+
+    describe("isInSeason", function(){
+        it('first day of the year', function() {
+            isInSeason('2018-01-01').should.be.false
+        })
+        it('a day before the season', function() {
+            isInSeason('2018-02-15').should.be.false
+        })
+
+        it('first day of the season', function() {
+            isInSeason('2018-04-15').should.be.true
+        })
+
+        it('a day in the season', function() {
+            isInSeason('2018-07-15').should.be.true
+        })
+        it('last day of the season', function() {
+            isInSeason('2018-10-15').should.be.true
+        })
+        it('first day after the season', function() {
+            isInSeason('2018-10-16').should.be.false
+        })
+
+        it('a day after the season', function() {
+            isInSeason('2018-11-15').should.be.false
+        })
+        it('the last day of the year', function() {
+            isInSeason('2018-11-15').should.be.false
+        })    
+    })
+
 })
