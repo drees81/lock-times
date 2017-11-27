619a8f7bce8bc48df84c7d9a36803c81c0cd657e
Jan Drees <jan@drees81.de>
2017-11-27 22:16:53 +0100

    added createDaySchedule function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const seasonStart = new Date("2000-04-15 00:00:00.0");
 const seasonEnd = new Date("2000-10-15 00:00:00.0");
 
 module.exports = {
+
     calcLockSchedule: function(date) {
         if (module.exports.isInSeason(date)) {
             return ['x']
@@ -16,5 +17,23 @@ module.exports = {
         date.setHours(0, 0 ,0 ,0);
         
         return date >= seasonStart && date <= seasonEnd;
+    },
+
+    createDaySchedule: function(isoDate, startHour, endHour, onlyEvenHours, skipNoon) {
+        const result = [];
+        const date = new Date(isoDate)
+
+        for (hour=startHour; hour<=endHour; hour++) {
+            if ( (isEven(hour) || !onlyEvenHours) && (hour!=12 || !skipNoon)) {
+                date.setHours(hour)
+                result.push(date.toJSON())
+            }
+        }
+
+        return result;
     }
 };
+
+function isEven(n) {
+    return n % 2 == 0;
+}
--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ var should = require('chai').should()
 var lockTimeCalculator = require('../index')
 var calcLockSchedule = lockTimeCalculator.calcLockSchedule
 var isInSeason = lockTimeCalculator.isInSeason
+var createDaySchedule = lockTimeCalculator.createDaySchedule
 
 describe("calcSchedule - out of season", function(){
     it('last day before the season', function() {
@@ -43,6 +44,33 @@ describe("helper functions", function() {
         it('the last day of the year', function() {
             isInSeason('2018-11-15').should.be.false
         })    
+    }),
+
+    describe("createDaySchedule", function() {
+        it('create all hours', () => {
+            createDaySchedule('2018-06-01', 8, 12, false, false ).should.eql([
+                '2018-06-01T06:00:00.000Z', 
+                '2018-06-01T07:00:00.000Z',
+                '2018-06-01T08:00:00.000Z',
+                '2018-06-01T09:00:00.000Z', 
+                '2018-06-01T10:00:00.000Z'
+            ])
+        })
+        it('create only even hours', () => {
+            createDaySchedule('2018-05-31', 8, 12, true, false ).should.eql([
+                '2018-05-31T06:00:00.000Z', 
+                '2018-05-31T08:00:00.000Z',
+                '2018-05-31T10:00:00.000Z'
+            ])
+        })
+        it('create only even hours and skip noon', () => {
+            createDaySchedule('2018-05-31', 10, 14, false, true ).should.eql([
+                '2018-05-31T08:00:00.000Z', 
+                '2018-05-31T09:00:00.000Z',
+                '2018-05-31T11:00:00.000Z',
+                '2018-05-31T12:00:00.000Z'
+            ])
+        })
     })
 
 })
